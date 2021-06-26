0844e6f4b7f905ebdae94658f298a1b399df63d6
TzuchingWu <73369062+TzuchingWu@users.noreply.github.com>
2021-06-26 15:14:26 -0700

    added navig

--- app/src/main/java/com/example/tritonhacks2021/Tasks.java
+++ app/src/main/java/com/example/tritonhacks2021/Tasks.java
@@ -3,12 +3,17 @@ package com.example.tritonhacks2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Tasks extends AppCompatActivity {
 
+    Button nextButton;
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        nextButton= findViewById(R.id.)
     }
 }
--- app/src/main/java/com/example/tritonhacks2021/setting.java
+++ app/src/main/java/com/example/tritonhacks2021/setting.java
@@ -2,13 +2,36 @@ package com.example.tritonhacks2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class setting extends AppCompatActivity {
 
+    Button nextButton;
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        nextButton= findViewById(R.id.button_nextsetting_setting);
+        backButton= findViewById(R.id.button_backsetting_setting);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(setting.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(setting.this, Tasks.class);
+                startActivity(intent);
+            }
+        });
     }
 }
