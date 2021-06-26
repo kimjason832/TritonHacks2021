f6411f807d4a0ad45685146e8da4d68b73df7778
TzuchingWu <73369062+TzuchingWu@users.noreply.github.com>
2021-06-26 16:46:55 -0700

    Update Timer.java

--- app/src/main/java/com/example/tritonhacks2021/Timer.java
+++ app/src/main/java/com/example/tritonhacks2021/Timer.java
@@ -2,13 +2,42 @@ package com.example.tritonhacks2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Timer extends AppCompatActivity {
+
+    Button backButton;
+    Button homeButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        backButton=findViewById(R.id.button_back_timer);
+        homeButton=findViewById(R.id.button_home_timer);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Timer.this, Tasks.class);
+                startActivity(intent);
+            }
+        });
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Timer.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
